83adfd5b691fa1ce44d0ccf5aba5d0e03e27e4e8
Bloodraze <venom228hysu2281@mail.ru>
2025-11-14 16:11:11 +0300

    14.11(2)

--- animal.php
+++ animal.php
@@ -8,9 +8,11 @@
         class User {
             protected $name;
             protected $age;
+            private $role;
             public function __construct($name, $age) {
                 $this->name = $this->correctName($name);
                 $this->age = $age;
+                $this->role = 'User';
             }
             public function getName() {
                 return $this->name;
@@ -24,8 +26,8 @@
             public function setAge($age) {
                 $this->age = $age;
             }
-            public function sayAboutMe() {
-                echo "Меня зовут <b>{$this->name}</b>, мне <b>{$this->age}</b> лет.<br>";
+            public function getRole() {
+                return $this->role;
             }
             protected function correctName($name) {
                 $name = strip_tags($name);
@@ -34,14 +36,30 @@
                 }
                 return $name;
             }
+            public static function makeAdmin($user) {
+                $user->role = 'Admin';
+            }
+            public static function createAdmin($name, $age) {
+                $admin = new User($name, $age);
+                $admin->role = 'Admin';
+                return $admin;
+            }
+            public function sayAboutMe() {
+                echo "Меня зовут <b>{$this->name}</b>, мне <b>{$this->age}</b> лет, роль: <b>{$this->role}</b>.<br>";
+            }
         }
         class Student extends User {
             private $course;
             private $groupe;
+            private static $numberStudents = 0;
             public function __construct($name, $age, $course, $groupe) {
                 parent::__construct($name, $age);
                 $this->course = $course;
                 $this->groupe = $groupe;
+                self::$numberStudents++;
+            }
+            public function __destruct() {
+                self::$numberStudents--;
             }
             public function getCourse() {
                 return $this->course;
@@ -55,17 +73,34 @@
             public function setGroupe($groupe) {
                 $this->groupe = $groupe;
             }
+            public static function getnumberStudents() {
+                return self::$numberStudents;
+            }
+            public static function printStudentInfo($student) {
+                echo "Студент: <b>{$student->getName()}</b>, возраст: <b>{$student->getAge()}</b>, курс: <b>{$student->getCourse()}</b>, группа: <b>{$student->getGroupe()}</b>.<br>";
+            }
             public function sayAboutMe() {
                 parent::sayAboutMe();
                 echo "Курс: <b>{$this->course}</b>, Группа: <b>{$this->groupe}</b>.<br>";
             }
         }
-        $user = new User("Иван <b>Иванов</b>", 30);
-        $student1 = new Student("Аня Петрова", 20, 2, "Б123");
-        $student2 = new Student("Максим Сидоров", 22, 3, "А456");
-        $user->sayAboutMe();
-        $student1->sayAboutMe();
-        $student2->sayAboutMe();
+        $students = [
+            new Student("Аня Петрова", 20, 2, "Б123"),
+            new Student("Максим Сидоров", 22, 3, "А456"),
+            new Student("Елена Смирнова", 19, 1, "В789"),
+            new Student("Дмитрий Козлов", 21, 4, "Г321"),
+            new Student("Ольга Иванова", 23, 5, "Д654"),
+        ];
+        echo "Количество студентов: ", '<b>', Student::getnumberStudents(),'</b>', "<br>";
+        unset($students[0], $students[1]);
+        echo "Количество студентов после удаления: ", '<b>', Student::getnumberStudents(), '</b>', "<br>";
+        foreach ($students as $student) {
+            Student::printStudentInfo($student);
+        }
+        User::makeAdmin($students[2]);
+        echo "Роль студента {$students[2]->getName()}: ", '<b>', $students[2]->getRole(), '</b>', "<br>";
+        $admin = User::createAdmin("Администратор Новиков", 40);
+        $admin->sayAboutMe();
         ?>
     </body>
 </html>
