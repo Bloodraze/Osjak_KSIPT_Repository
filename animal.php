68decd8cfd6f7aa523e064f7eb1b5c20c283a41a
Bloodraze <venom228hysu2281@mail.ru>
2025-11-20 15:55:09 +0300

    20.11

--- animal.php
+++ animal.php
@@ -8,7 +8,7 @@
         class User {
             protected $name;
             protected $age;
-            private $role;
+            protected $role;
             public function __construct($name, $age) {
                 $this->name = $this->correctName($name);
                 $this->age = $age;
@@ -36,14 +36,6 @@
                 }
                 return $name;
             }
-            public static function makeAdmin($user) {
-                $user->role = 'Admin';
-            }
-            public static function createAdmin($name, $age) {
-                $admin = new User($name, $age);
-                $admin->role = 'Admin';
-                return $admin;
-            }
             public function sayAboutMe() {
                 echo "Меня зовут <b>{$this->name}</b>, мне <b>{$this->age}</b> лет, роль: <b>{$this->role}</b>.<br>";
             }
@@ -56,6 +48,7 @@
                 parent::__construct($name, $age);
                 $this->course = $course;
                 $this->groupe = $groupe;
+                $this->role = 'Студент';
                 self::$numberStudents++;
             }
             public function __destruct() {
@@ -64,43 +57,85 @@
             public function getCourse() {
                 return $this->course;
             }
-            public function setCourse($course) {
-                $this->course = $course;
-            }
             public function getGroupe() {
                 return $this->groupe;
-            }
-            public function setGroupe($groupe) {
-                $this->groupe = $groupe;
             }
             public static function getnumberStudents() {
                 return self::$numberStudents;
             }
-            public static function printStudentInfo($student) {
-                echo "Студент: <b>{$student->getName()}</b>, возраст: <b>{$student->getAge()}</b>, курс: <b>{$student->getCourse()}</b>, группа: <b>{$student->getGroupe()}</b>.<br>";
-            }
             public function sayAboutMe() {
+                echo "Я студент.<br>";
                 parent::sayAboutMe();
                 echo "Курс: <b>{$this->course}</b>, Группа: <b>{$this->groupe}</b>.<br>";
             }
         }
-        $students = [
-            new Student("Аня Петрова", 20, 2, "Б123"),
-            new Student("Максим Сидоров", 22, 3, "А456"),
-            new Student("Елена Смирнова", 19, 1, "В789"),
-            new Student("Дмитрий Козлов", 21, 4, "Г321"),
-            new Student("Ольга Иванова", 23, 5, "Д654"),
-        ];
-        echo "Количество студентов: ", '<b>', Student::getnumberStudents(),'</b>', "<br>";
-        unset($students[0], $students[1]);
-        echo "Количество студентов после удаления: ", '<b>', Student::getnumberStudents(), '</b>', "<br>";
-        foreach ($students as $student) {
-            Student::printStudentInfo($student);
+        class Teacher extends User {
+            private $subjects;
+            public function __construct($name, $age, $subjects=[]) {
+                parent::__construct($name, $age);
+                $this->subjects = $subjects;
+                $this->role = 'Преподаватель';
+            }
+            public function getSubjects() {
+                return $this->subjects;
+            }
+            public function setSubjects($subjects) {
+                $this->subjects = $subjects;
+            }
+            public function sayAboutMe() {
+                echo "Я преподаватель.<br>";
+                parent::sayAboutMe();
+                echo "Веду предметы: <b>", implode(', ', $this->subjects), "</b>.<br>";
+            }
         }
-        User::makeAdmin($students[2]);
-        echo "Роль студента {$students[2]->getName()}: ", '<b>', $students[2]->getRole(), '</b>', "<br>";
-        $admin = User::createAdmin("Администратор Новиков", 40);
-        $admin->sayAboutMe();
+        class Manager extends User {
+            private $position;
+            private $jobDuties;
+            public function __construct($name, $age, $position, $jobDuties=[]) {
+                parent::__construct($name, $age);
+                $this->position = $position;
+                $this->jobDuties = $jobDuties;
+                $this->role = 'Администрация';
+            }
+            public function getPosition() {
+                return $this->position;
+            }
+            public function setPosition($position) {
+                $this->position = $position;
+            }
+            public function getJobDuties() {
+                return $this->jobDuties;
+            }
+            public function setJobDuties($duties) {
+                $this->jobDuties = $duties;
+            }
+            public function sayAboutMe() {
+                echo "Я сотрудник администрации.<br>";
+                parent::sayAboutMe();
+                echo "Должность: <b>{$this->position}</b>.<br>";
+                echo "Обязанности: <b>", implode(', ', $this->jobDuties), "</b>.<br>";
+            }
+        }
+        $persons = [
+            new Student("Анна Петрова", 20, 2, "Б123"),
+            new Teacher("Иван Иванов", 45, ["Математика", "Физика"]),
+            new Manager("Сергей Смирнов", 38, "Директор", ["Управление", "Контроль", "Организация"]),
+            new Student("Максим Сидоров", 22, 3, "А456"),
+            new Teacher("Мария Кузнецова", 34, ["История"]),
+            new Manager("Елена Новикова", 30, "Зам. директора", ["Документооборот", "Координация"]),
+            new Student("Ольга Иванова", 21, 1, "В789")
+        ];
+        usort($persons, function($a, $b) {
+            return strcmp($a->getName(), $b->getName());
+        });
+        foreach ($persons as $person) {
+            $person->sayAboutMe();
+            echo "<hr>";
+        }
+        $winnerIndex = rand(0, count($persons) - 1);
+        $winner = $persons[$winnerIndex];
+        echo "<h3>Победитель розыгрыша:</h3>";
+        $winner->sayAboutMe();
         ?>
     </body>
 </html>
