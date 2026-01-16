7f27848f04975372f7ede4ec3309b0a1ff5f51b3
Bloodraze <venom228hysu2281@mail.ru>
2026-01-16 15:26:45 +0300

    15.01.26

--- Course 2/Term2/Practice_Works/Practice Work 2 (15.01.26)/Task 4/task4.js
+++ Course 2/Term2/Practice_Works/Practice Work 2 (15.01.26)/Task 4/task4.js
@@ -3,13 +3,14 @@ const a = Number(readline.question("Input a: "));
 const b = Number(readline.question("Input b: "));
 const c = Number(readline.question("Input c: "));
 const x = Number(readline.question("Input x: "));
+let y = 0;
 if (x<0){
-    let y = (a+b);
+    y = a+b;
     console.log(y);
 }else if(x>0){
-    let y = c/b;
+    y = c/b;
     console.log(y);
 }else{
-    let y = c*(a+2*b);
+    y = c*(a+2*b);
     console.log(y);
 }
