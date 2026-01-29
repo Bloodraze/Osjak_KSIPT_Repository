8cb41b027765061e83b062ad9c8d328bb36584e2
Bloodraze <venom228hysu2281@mail.ru>
2026-01-29 16:21:56 +0300

    29.01.26

--- Course 2/Term2/Lessons/JavaScript (January - )/January/29.01.26/script.js
+++ Course 2/Term2/Lessons/JavaScript (January - )/January/29.01.26/script.js
@@ -1,11 +1,20 @@
-const x = '10';
-switch(x*2){
-    case 10:
-        console.log('Число 10');
-        break;
-    case 20:
-        console.log('Число 10');
-        break;
-    default:
-        console.log('Ничего не совпало');
-}
+//SWITCH
+// const x = '10';
+// switch(x*2){
+//     case 10:
+//         console.log('Число 10');
+//         break;
+//     case 20:
+//         console.log('Число 10');
+//         break;
+//     default:
+//         console.log('Ничего не совпало');
+// }
+
+//МАССИВЫ
+let array = [4, 5, 34, 'string', true];
+//array[3] = 4423; //замена 'string' -> 4423
+let count = array.length;
+//array[8] = 4423; //добавление 4423
+array.push(4423); //добавление 4423
+console.log(array);
